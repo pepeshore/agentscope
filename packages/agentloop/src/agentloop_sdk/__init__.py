728cc5f7a35e1ecdd66eaedbb764976106007676
wu.cc <wu.cc@alibaba-inc.com>
2026-03-13 11:09:36 +0800

    支持agentloop-sdk单独打包
    Change-Id: I3557d4ba3056c459de7d7d758046393029572a52

---
+++ packages/agentloop/src/agentloop_sdk/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""AgentLoop SDK: Evaluation tools for the AgentLoop platform."""
+
+from agentscope.evaluate import (
+    AgentLoopConfig,
+    AgentLoopBenchmark,
+    AgentLoopEvaluatorStorage,
+    Task,
+    SolutionOutput,
+    BenchmarkBase,
+    EvaluatorStorageBase,
+    FileEvaluatorStorage,
+    GeneralEvaluator,
+    RayEvaluator,
+    MetricBase,
+    MetricResult,
+    MetricType,
+)
+
+__all__ = [
+    "AgentLoopConfig",
+    "AgentLoopBenchmark",
+    "AgentLoopEvaluatorStorage",
+    "Task",
+    "SolutionOutput",
+    "BenchmarkBase",
+    "EvaluatorStorageBase",
+    "FileEvaluatorStorage",
+    "GeneralEvaluator",
+    "RayEvaluator",
+    "MetricBase",
+    "MetricResult",
+    "MetricType",
+]
